2dad51a153fd77d2dc3b517e77751b91d24259ea
leo <leonardoandrei41@gmail.com>
2024-07-03 18:20:25 -0300

    Implement a basic write-a-verb mode of the game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Exquisite Corpse");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Write a verb (third person form): It... ");
+    String word = scanner.nextLine();
+    System.out.printf("The big bird %s the scared Rabbit.", word);
+    scanner.close();
+  }
 }
